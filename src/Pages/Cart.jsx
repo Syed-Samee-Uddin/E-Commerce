193d6c7c36c4f99ccacc567faf8f08fdd08ab851
Syed-Samee-Uddin <155414849+Syed-Samee-Uddin@users.noreply.github.com>
2024-06-15 11:19:50 +0400

    Update Cart.jsx

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './CSS/Cart.css'
+import Footer from '../Components/Footer/Footer'
 
 export const Cart = () => {
   return (
-    <div>Cart</div>
-  )
+<div className="cart">
+  <table>
+    <tr>
+      <th>Product</th>
+      <th>Quantity</th>
+      <th>Subtotal</th>
+    </tr>
+    <td className='ab'>Null</td>
+    <td><input className='abc' type="number" value="0" /></td>
+    <td>$0</td>
+  </table>
+</div>  )
 }
+export default Cart
